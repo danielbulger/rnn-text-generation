Training now writes to a summary to be explored with TensorBoard. Fixed the batch culling as it was reporting incorrect values

main.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 
+import pickle
 import torch
 import numpy as np
 
@@ -18,7 +19,6 @@ def parse_args():
 
 
 def read_chars(in_file):
-    import pickle
 
     with open(in_file, 'rb') as input_file:
         return pickle.load(input_file)
@@ -32,14 +32,11 @@ def main():
     characters = chars['characters']
     character_index = chars['character_index']
 
-    use_cuda = torch.cuda.is_available()
-    device = torch.device('cuda' if use_cuda else 'cpu')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     model = torch.load(args.model)
     model.eval()
-
-    if use_cuda:
-        model = model.to(device)
+    model = model.to(device)
 
     output = [args.start]
 
@@ -56,13 +53,10 @@ def main():
     for x in range(args.count):
         # Stack as we need a batch dimension of 1
         current = torch.stack([make_one_hot(vocab_size, character_index[output[-1]])])
-
-        if use_cuda:
-            current = current.to(device)
+        current = current.to(device)
 
         # The soft-max probabilities
         prediction, (state_hidden1, state_context1), (state_hidden2, state_context2) = model(current, (state_hidden1, state_context1), (state_hidden2, state_context2))
-
 
         # Sample from the top N most probable predictions.
         _, top_index = torch.topk(prediction, k=10)

rnn/CharDataset.py
@@ -1,5 +1,6 @@
 import torch
 from torch.utils import data
+from math import floor
 
 
 def make_one_hot(vocab_size, index):
@@ -7,42 +8,38 @@ def make_one_hot(vocab_size, index):
     x[index] = 1
     return torch.tensor(x)
 
+def cull_to_batch_size(content, batch_size):
+
+    n = len(content)
+
+    # Already a multiple of the batch size
+    if n % batch_size == 0:
+        return content
+
+    # Divide and round down so we end up at an even number
+    # then times back up so we are the nearest lower multiple of
+    # batch_size
+    x = floor(n / batch_size) * batch_size
+
+    return content[: x + 1]
+
 
 class CharDataset(data.Dataset):
-
-    @staticmethod
-    def convert_to_batch_set(content, batch_size):
-        n = len(content)
-
-        quot = n % batch_size
-
-        # The content length is a multiple if batch size.
-        if quot  == 0:
-            return content
-
-        # Otherwise find the closest lower multiple.
-        n -= quot
-
-        # +1 since we need to look ahead 1 more character
-        return content[: n + 1]
-
 
     def __init__(self, batch_size, vocab_size, content, character_index):
         super(CharDataset, self).__init__()
         self.vocab_size = vocab_size
-        self.content = CharDataset.convert_to_batch_set(content, batch_size)
-
+        self.content = cull_to_batch_size(content, batch_size)
         # -1 since we can't look forward when there is nothing left.
-        self.length = len(content) - 1
+        self.length = len(self.content) - 1
         self.character_index = character_index
 
     def __getitem__(self, index):
-
         # Get the training one hot encoding
         train = make_one_hot(self.vocab_size, self.character_index[self.content[index]])
 
         # For the Cross Entropy we use the index not the one-hot encoding as the target.
-        target = torch.tensor([self.character_index[self.content[index + 1]]])
+        target = torch.tensor([self.character_index[self.content[index + 1]]  if index < self.length - 1else 0])
 
         return train.type(torch.LongTensor), target.type(torch.LongTensor)
 

rnn/Encoder.py
@@ -21,8 +21,7 @@ class Encoder(nn.Module):
         ]
 
     def forward(self, x, prev_state1, prev_state2):
-        batch_size = x.size(0)
-
+        
         embedding = self.embedding(x)
     
         rnn1, (hidden1, context1) = self.rnn1(embedding, (prev_state1[0], prev_state1[1]))

train.py
@@ -8,6 +8,7 @@ import multiprocessing
 
 import torch.nn.functional as F
 from torch.nn import CrossEntropyLoss
+from torch.utils.tensorboard import SummaryWriter
 
 from rnn.CharDataset import CharDataset
 from rnn.Encoder import Encoder
@@ -17,7 +18,7 @@ def get_args():
     parser = ArgumentParser()
     parser.add_argument('--input', type=str, required=True)
     parser.add_argument('--epochs', type=int, required=True)
-    parser.add_argument('--embedding-size', type=int, default=100)
+    parser.add_argument('--embedding-size', type=int, default=256)
     parser.add_argument('--rnn1-size', type=int, default=128)
     parser.add_argument('--rnn2-size', type=int, default=128)
     parser.add_argument('--lr', type=float, default=0.001)
@@ -85,6 +86,8 @@ def main():
         num_workers=args.workers
     )
 
+    writer = SummaryWriter(os.path.join(args.log_dir))
+
     for epoch in range(args.epochs):
 
         running_loss = 0.0
@@ -120,12 +123,17 @@ def main():
             # Sum the loss from this batch into the checkpoint total
             running_loss += loss.item()
 
-            if index != 0 and index % args.checkpoint == 0:
-                checkpoint(model, args.log_dir, "checkpoint-{}-{}.pth".format(epoch, index))
-                print('[%d,%d] loss: %.6f' % (epoch, index, running_loss / args.batch_size))
+            if index % args.checkpoint == args.checkpoint - 1:
+
+                writer.add_scalar(
+                    'training loss', running_loss, epoch * len(data_loader) + index
+                )
+
                 running_loss = 0.0
 
     checkpoint(model, args.log_dir, "final.pth")
+
+    writer.close()
 
 
 if __name__ == '__main__':