Reworked model as the previous one was failing to converge, replaced LSTM with GRU, fixed incorrect dimension for the softmax in main and CharDataset will now include a sequence & batch

main.py
@@ -1,12 +1,10 @@
-
-import pickle
 import torch
-import numpy as np
 import torch.nn.functional as F
 
 from argparse import ArgumentParser
 from torch.distributions import Categorical
-from rnn.CharDataset import make_one_hot
+
+from rnn.CharDataset import CharDataset
 
 
 def parse_args():
@@ -21,19 +19,13 @@ def parse_args():
     return parser.parse_args()
 
 
-def read_chars(in_file):
-
-    with open(in_file, 'rb') as input_file:
-        return pickle.load(input_file)
-
-
 def main():
     args = parse_args()
 
-    chars = read_chars(args.chars)
-    vocab_size = chars['count']
-    characters = chars['characters']
-    character_index = chars['character_index']
+    characters = CharDataset.deserialize(args.chars)
+
+    vocab_to_index = characters['vocab_to_index']
+    index_to_vocab = characters['index_to_vocab']
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -43,33 +35,25 @@ def main():
 
     output = [args.start]
 
-    state_hidden1, state_context1 = model.zero_state(1, model.rnn1_hidden_size)
-    state_hidden2, state_context2 = model.zero_state(1, model.rnn2.hidden_size)
-
-    state_hidden1 = state_hidden1.to(device)
-    state_context1 = state_context1.to(device)
-
-    state_hidden2 = state_hidden2.to(device)
-    state_context2 = state_context2.to(device)
-
+    state_hidden = model.zero_state(1).to(device)
 
     for x in range(args.count):
-        # Stack as we need a batch dimension of 1
-        current = torch.stack([make_one_hot(vocab_size, character_index[output[-1]])])
-        current = current.to(device)
+
+        current = torch.tensor([[vocab_to_index[output[-1]]]]).to(device)
 
         # The soft-max probabilities
-        prediction, (state_hidden1, state_context1), (state_hidden2, state_context2) = model(current, (state_hidden1, state_context1), (state_hidden2, state_context2))
+        prediction, state_hidden = model(current, state_hidden)
 
+        prediction = prediction[0]
 
         # Scale by the temperature
-        prediction = F.softmax(prediction[0], dim=0) / args.temp
+        prediction = F.softmax(prediction / args.temp, dim=1)
 
         dist = Categorical(prediction)
         index = dist.sample()
 
         # Convert the probabilities to the character
-        character = characters[index]
+        character = index_to_vocab[index[0].item()]
 
         output.append(character)
 

rnn/CharDataset.py
@@ -1,47 +1,62 @@
-import torch
-from torch.utils import data
+import pickle
 from math import floor
 
-
-def make_one_hot(vocab_size, index):
-    x = [0] * vocab_size
-    x[index] = 1
-    return torch.tensor(x)
-
-def cull_to_batch_size(content, batch_size):
-
-    n = len(content)
-
-    # Already a multiple of the batch size
-    if n % batch_size == 0:
-        return content
-
-    # Divide and round down so we end up at an even number
-    # then times back up so we are the nearest lower multiple of
-    # batch_size
-    x = floor(n / batch_size) * batch_size
-
-    return content[: x + 1]
+import torch
+from torch.utils import data
+from collections import Counter
 
 
 class CharDataset(data.Dataset):
 
-    def __init__(self, batch_size, vocab_size, content, character_index):
+    @staticmethod
+    def read_tokens(content):
+        counts = Counter(content)
+        sorted_vocab = sorted(counts, key=counts.get, reverse=True)
+
+        index_to_vocab = {k: v for k, v in enumerate(sorted_vocab)}
+        vocab_to_index = {v: k for k, v in enumerate(sorted_vocab)}
+
+        indexed = [vocab_to_index[v] for v in content]
+        return index_to_vocab, vocab_to_index, indexed
+
+    @staticmethod
+    def from_file(file_path, sequence_size):
+        with open(file_path) as input_file:
+            content = input_file.read()
+            return CharDataset(content, sequence_size)
+
+    @staticmethod
+    def deserialize(file_path):
+        with open(file_path, 'rb') as input_file:
+            return pickle.load(input_file)
+
+    def __init__(self, content, sequence_size):
         super(CharDataset, self).__init__()
-        self.vocab_size = vocab_size
-        self.content = cull_to_batch_size(content, batch_size)
-        # -1 since we can't look forward when there is nothing left.
-        self.length = len(self.content) - 1
-        self.character_index = character_index
+        self.length = len(content) - sequence_size
+        self.sequence_size = sequence_size
+        self.index_to_vocab, self.vocab_to_index, self.indexed = CharDataset.read_tokens(content)
 
     def __getitem__(self, index):
-        # Get the training one hot encoding
-        train = make_one_hot(self.vocab_size, self.character_index[self.content[index]])
-
-        # For the Cross Entropy we use the index not the one-hot encoding as the target.
-        target = torch.tensor([self.character_index[self.content[index + 1]]  if index < self.length - 1else 0])
-
-        return train.type(torch.LongTensor), target.type(torch.LongTensor)
+        train = self.indexed[index: index + self.sequence_size]
+        target = self.indexed[index + 1: index + 1 + self.sequence_size]
+        return torch.LongTensor(train), torch.LongTensor(target)
 
     def __len__(self):
         return self.length
+
+    def serialize(self, file_path):
+        with open(file_path, 'wb') as output_file:
+            pickle.dump({
+                'count': self.get_vocab_size(),
+                'index_to_vocab': self.index_to_vocab,
+                'vocab_to_index': self.vocab_to_index
+            }, output_file)
+
+    def get_vocab_size(self) -> int:
+        return len(self.index_to_vocab)
+
+    def get_vocab(self, index: int) -> str:
+        return self.index_to_vocab[index]
+
+    def get_index(self, vocab: str) -> int:
+        return self.vocab_to_index[vocab]

rnn/Encoder.py
@@ -4,34 +4,23 @@ from torch import nn
 
 class Encoder(nn.Module):
 
-    def __init__(self, vocab_size, embedding_size, rnn1_size, rnn2_size):
+    def __init__(self, vocab_size, embedding_size, rnn_size, num_layers, dropout_prob):
         super(Encoder, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_size)
-        self.rnn1 = nn.LSTM(embedding_size, rnn1_size, 1, batch_first=True)
-        self.rnn2 = nn.LSTM(rnn1_size, rnn2_size, 1, batch_first=True)
-        self.rnn1_hidden_size = rnn1_size
-        self.rnn2_hidden_size = rnn2_size
-        self.dense = nn.Linear(embedding_size + rnn1_size + rnn2_size, vocab_size)
+        self.rnn = nn.GRU(embedding_size, rnn_size, num_layers, dropout=dropout_prob, batch_first=True)
+        self.decoder = nn.Linear(embedding_size, vocab_size)
+        self.num_layers = num_layers
+        self.rnn_size = rnn_size
 
-    def zero_state(self, batch_size, size):
+    def zero_state(self, batch_size):
         # One for the hidden state & one for contextual state.
-        return [
-            torch.zeros(1, batch_size, size),
-            torch.zeros(1, batch_size, size)
-        ]
+        return torch.zeros(self.num_layers, batch_size, self.rnn_size)
 
-    def forward(self, x, prev_state1, prev_state2):
-        
-        embedding = self.embedding(x)
-    
-        rnn1, (hidden1, context1) = self.rnn1(embedding, (prev_state1[0], prev_state1[1]))
-        rnn2, (hidden2, context2) = self.rnn2(rnn1, (prev_state2[0], prev_state2[1]))
+    def forward(self, x, state):
+        x = self.embedding(x)
 
-        embedding = embedding.transpose(0, 1)[-1]
-        rnn1 = rnn1.transpose(0, 1)[-1]
-        rnn2 = rnn2.transpose(0, 1)[-1]
+        x, hidden = self.rnn(x, state)
 
-        x = torch.cat([embedding, rnn1, rnn2], dim=1)
-        x = self.dense(x)
+        x = self.decoder(x)
 
-        return (x, [hidden1, context1], [hidden2, context2])
+        return x, hidden

train.py
@@ -1,16 +1,11 @@
 import os
 import re
 
-import pickle
 import torch
 import torch.utils.data
 import multiprocessing
 
-import torch.nn.functional as F
-
 from argparse import ArgumentParser
-
-from torch.nn import CrossEntropyLoss
 from torch.utils.tensorboard import SummaryWriter
 
 from rnn.CharDataset import CharDataset
@@ -22,24 +17,18 @@ def get_args():
     parser.add_argument('--input', type=str, required=True)
     parser.add_argument('--epochs', type=int, required=True)
     parser.add_argument('--embedding-size', type=int, default=256)
-    parser.add_argument('--rnn1-size', type=int, default=512)
-    parser.add_argument('--rnn2-size', type=int, default=512)
-    parser.add_argument('--lr', type=float, default=2e-3)
-    parser.add_argument('--batch-size', type=int, default=32)
+    parser.add_argument('--rnn-size', type=int, default=512)
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--batch-size', type=int, default=128)
+    parser.add_argument('--sequence-size', type=int, default=32)
     parser.add_argument('--workers', type=int)
     parser.add_argument('--log-dir', default="./log/", type=str)
     parser.add_argument('--checkpoint', default=50, type=int)
     parser.add_argument('--chars', default="./log/chars.pk", type=str)
     parser.add_argument('--model', type=str)
+    parser.add_argument('--num-layers', type=int, default=2)
+    parser.add_argument('--dropout', type=float, default=0.02)
     return parser.parse_args()
-
-
-def get_chars(file):
-    with open(file) as input_file:
-        content = input_file.read()
-        characters = sorted(list(set(content)))
-        character_index = {c: i for i, c in enumerate(characters)}
-        return content, len(characters), characters, character_index
 
 
 def checkpoint(model, directory, file):
@@ -47,24 +36,14 @@ def checkpoint(model, directory, file):
     torch.save(model, path)
 
 
-def write_chars(file, vocab_size, characters, character_index):
-    with open(file, 'wb') as output_file:
-        data = {
-            'count': vocab_size,
-            'characters': characters,
-            'character_index': character_index
-        }
-
-        pickle.dump(data, output_file)
-
 def get_checkpoint_model(model_path):
     model = torch.load(model_path)
     model.eval()
-    model.train()
 
-    m = re.search('(\d+).pth', model_path)
-    
-    return model, int(m.group(1)) + 1
+    m = re.search(r'(\d+).pth', model_path)
+
+    return model, int(m.group(1))
+
 
 def main():
     args = get_args()
@@ -76,14 +55,10 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     if torch.cuda.is_available():
-         torch.backends.cudnn.fastest = True
+        torch.backends.cudnn.fastest = True
 
-    content, vocab_size, characters, character_index = get_chars(args.input)
-
-    # Write the required training data to file.
-    write_chars(
-        args.chars, vocab_size, characters, character_index
-    )
+    dataset = CharDataset.from_file(args.input, args.sequence_size)
+    dataset.serialize(args.chars)
 
     epoch = 0
 
@@ -92,71 +67,63 @@ def main():
         model, epoch = get_checkpoint_model(args.model)
     else:
         model = Encoder(
-            vocab_size, args.embedding_size,  args.rnn1_size, args.rnn2_size
+            dataset.get_vocab_size(), args.embedding_size, args.rnn_size, args.num_layers, args.dropout
         )
 
     model = model.to(device)
+    model.train()
 
     optimiser = torch.optim.Adam(model.parameters(), lr=args.lr)
+    criterion = torch.nn.CrossEntropyLoss()
 
     data_loader = torch.utils.data.DataLoader(
-        CharDataset(args.batch_size, vocab_size, content, character_index),
+        dataset,
         batch_size=args.batch_size,
         shuffle=False,
         num_workers=args.workers
     )
 
-    writer = SummaryWriter(os.path.join(args.log_dir))
+    writer = SummaryWriter(args.log_dir)
 
     for epoch in range(epoch, args.epochs):
 
         running_loss = 0.0
 
-        state_hidden1, state_context1 = model.zero_state(args.batch_size, args.rnn1_size) 
-        state_hidden2, state_context2 = model.zero_state(args.batch_size, args.rnn2_size)
-
-        state_hidden1 = state_hidden1.to(device)
-        state_context1 = state_context1.to(device)
-
-        state_hidden2 = state_hidden2.to(device)
-        state_context2 = state_context2.to(device)
+        state_hidden = model.zero_state(args.batch_size).to(device)
 
         print('Starting epoch {}...'.format(epoch + 1))
 
         for index, (train, labels) in enumerate(data_loader):
 
+            if len(train) != args.batch_size or len(labels) != args.batch_size:
+                continue
+
             train = train.to(device)
             labels = labels.to(device)
 
+            predict, state = model(train, state_hidden)
+
+            loss = criterion(predict.transpose(1, 2), labels)
+
+            state_hidden = state.detach()
+
             optimiser.zero_grad()
-            predict, state1, state2 = model(train, (state_hidden1, state_context1), (state_hidden2, state_context2))
-            
-            loss = F.cross_entropy(predict, labels.squeeze())
-
-            state_hidden1 = state1[0].detach()
-            state_context1 = state1[1].detach()
-
-            state_hidden2 = state2[0].detach()
-            state_context2 = state2[1].detach()
-
             loss.backward()
-            optimiser.step()
             torch.nn.utils.clip_grad_norm_(model.parameters(), 5)
+            optimiser.step()
 
             # Sum the loss from this batch into the checkpoint total
             running_loss += loss.item()
 
             if index % args.checkpoint == args.checkpoint - 1:
-
                 writer.add_scalar(
-                    'training loss', running_loss, epoch * len(data_loader) + index
+                    'training loss', running_loss / args.batch_size, epoch * len(data_loader) + index
                 )
 
-                
                 checkpoint(model, args.log_dir, "{}_{}.pth".format(epoch, index))
                 running_loss = 0.0
 
-        checkpoint(model, args.log_dir, "{}.pth".format(epoch))
+        checkpoint(model, args.log_dir, "{}.pth".format(epoch + 1))
 
     checkpoint(model, args.log_dir, "final.pth")
 